148b0e800d1bc127a20e314bfd55136c9b2c82e3
suloginscene <suloginscene@gmail.com>
2021-03-02 17:12:52 +0900

    feat: Configure rest docs

--- src/test/java/com/github/suloginscene/authserver/member/api/MemberRestControllerTest.java
+++ src/test/java/com/github/suloginscene/authserver/member/api/MemberRestControllerTest.java
@@ -3,19 +3,24 @@ package com.github.suloginscene.authserver.member.api;
 import com.github.suloginscene.authserver.testing.Emails;
 import com.github.suloginscene.authserver.testing.Passwords;
 import com.github.suloginscene.authserver.testing.RequestSupporter;
+import com.github.suloginscene.authserver.testing.RestDocsConfig;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@AutoConfigureRestDocs @Import(RestDocsConfig.class)
 class MemberRestControllerTest {
 
     @Autowired MockMvc mockMvc;
@@ -25,11 +30,13 @@ class MemberRestControllerTest {
 
 
     @Test
-    void signup_onSuccess_returnsLocation() throws Exception {
+    void signup_onSuccess_returns201() throws Exception {
         SignupRequest request = new SignupRequest(Emails.VALID.get(), Passwords.VALID.get());
         ResultActions when = mockMvc.perform(requestSupporter.postWithJson(URL, request));
 
         ResultActions then = when.andExpect(status().isCreated());
+
+        then.andDo(document("signup"));
     }
 
 }
---
+++ src/test/java/com/github/suloginscene/authserver/testing/RestDocsConfig.java
@@ -0,0 +1,21 @@
+package com.github.suloginscene.authserver.testing;
+
+import org.springframework.boot.test.autoconfigure.restdocs.RestDocsMockMvcConfigurationCustomizer;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
+
+
+@TestConfiguration
+public class RestDocsConfig {
+
+    @Bean
+    RestDocsMockMvcConfigurationCustomizer restDocsMockMvcConfigurationCustomizer() {
+        return configurer -> configurer.operationPreprocessors()
+                .withRequestDefaults(prettyPrint())
+                .withResponseDefaults(prettyPrint());
+
+    }
+
+}
