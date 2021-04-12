c19dc5af284885cfce7bd9e8a2277086a846a220
suloginscene <suloginscene@gmail.com>
2021-04-12 22:47:21 +0900

    test: Add default member for developing frontend

---
+++ src/main/java/com/github/suloginscene/authserver/config/ManualTestConfigurer.java
@@ -0,0 +1,34 @@
+package com.github.suloginscene.authserver.config;
+
+import com.github.suloginscene.authserver.member.domain.Email;
+import com.github.suloginscene.authserver.member.domain.Member;
+import com.github.suloginscene.authserver.member.domain.MemberRepository;
+import com.github.suloginscene.authserver.member.domain.Password;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+import static com.github.suloginscene.profile.Profiles.LOCAL;
+
+
+@Component
+@Profile(LOCAL)
+@RequiredArgsConstructor
+public class ManualTestConfigurer implements ApplicationRunner {
+
+    private final MemberRepository memberRepository;
+    private final PasswordEncoder passwordEncoder;
+
+
+    @Override
+    public void run(ApplicationArguments args) {
+        String email = "scene@email.com";
+        String password = passwordEncoder.encode("test_pass");
+        Member member = new Member(new Email(email), new Password(password));
+        memberRepository.save(member);
+    }
+
+}
